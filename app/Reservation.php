cb727a10579685c6b533eb3381003ecb0b77152f
Alonso Avotz <alonso@avotz.com>
2017-04-24 18:56:44 -0600

    test travels

--- app/Reservation.php
+++ app/Reservation.php
@@ -7,11 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     protected $guarded = ['errors'];
-    protected $dates = [
-        'created_at',
-        'updated_at',
-        'date'
-    ];
+    
     public function scopeSearch($query, $search)
     {
         return $query->where(function ($query) use ($search)
